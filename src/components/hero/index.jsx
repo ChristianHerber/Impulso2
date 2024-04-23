2880ad4d8e1ce566705120430b0ade88cc94b4e8
Christian Herber <christianfaldox@gmail.com>
2024-04-23 10:40:58 -0400

    fix #3

--- src/components/hero/index.jsx
+++ src/components/hero/index.jsx
@@ -15,7 +15,7 @@ const Hero = () => {
             <div className={styles.hero__overlay}></div>
                 <video autoPlay muted playsInline className={styles.hero__video}>
                     <source src="/assets/marcio.mp4" type="video/mp4" />
-                    <Image src="/assets/marcio-bg.jpg" className={styles.hero__video} width={1320} height={548} alt="CTO - Márcio Sena" />
+                    <Image src="/assets/marcio-bg.svg" className={styles.hero__video} width={1320} height={548} alt="CTO - Márcio Sena" />
                 </video>
         </div>
 
